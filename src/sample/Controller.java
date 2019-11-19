177e90bf66a61c45055f2d75905116ddadbef927
Mevlut Arslan <mevlut12098@gmail.com>
2019-11-19 17:06:07 +0100

    implemented Breadth First Search with basic visualisation

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -7,14 +8,22 @@ import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Controller {
 
+    Node start;
+    Node end;
+
     public Canvas mainCanvas;
     public Button btn;
     public Text drawingText;
+    public Button bfs_btn;
+    public Button start_btn;
+
 
     @FXML
     void run(){
@@ -24,7 +33,6 @@ public class Controller {
     void drawNode(){
         mainCanvas.setOnMouseClicked(e -> {
             new Node(mainCanvas.getGraphicsContext2D(),e.getX(),e.getY());
-
         });
         drawingText.setText("Currently drawing : ");
         drawingText.setText(drawingText.getText() + " drawing node" );
@@ -35,48 +43,44 @@ public class Controller {
         GraphicsContext ctx = mainCanvas.getGraphicsContext2D();
         mainCanvas.setOnMouseClicked(e->{
             boolean isSelected = false;
-            double[] currentNode = new double[4];
-            for( double[] x : Node.nodes){
+            Node currentNode = null;
+            for( Node x : Node.nodes){
                 ctx.setFill(Color.LIGHTBLUE);
-                ctx.fillOval(x[0]-x[3]/2,x[1]-x[3]/2,x[3],x[3]);
+                ctx.fillOval(x.xCord-x.RADIUS,x.yCord-x.RADIUS,x.DIAMETER,x.DIAMETER);
                 //checking mouse location and comparing it to all the nodes in the canvas
-                if((e.getX() >= x[0] - x[3]/2 && e.getX() <= x[0])&& (e.getY() >= x[1] - x[3]/2&& e.getY() <= x[1])) {
-                    //if the mouse is at the location of a node we mark it as currentNode and then redraw it in RED to indicate its chosen
+                if((e.getX() >= x.xCord - x.RADIUS && e.getX() <= x.xCord)&& (e.getY() >= x.yCord - x.RADIUS&& e.getY() <= x.yCord)) {
+                    //if the mouse is clicked at the location of a node we mark it as currentNode and then redraw it in RED to indicate its chosen
                     isSelected = true;
                     currentNode = x;
-                    ctx.setFill(Color.WHITESMOKE);
-                    ctx.fillOval(x[0]-x[3]/2,x[1]-x[3]/2,x[3],x[3]);
-                    ctx.setFill(Color.RED);
-                    ctx.fillOval(x[0]-x[3]/2,x[1]-x[3]/2,x[3],x[3]);
+                    x.reDraw(ctx,Color.RED);
                 }
             }
-                //after making sure a node is selected then we use the currentNode to draw a line from its center to 
+                //after making sure a node is selected then we use the currentNode to draw a line from its center to the second selected Node
                 if(isSelected){
-                    double[] finalCurrentNode = currentNode;
+                    Node finalCurrentNode = currentNode;
                     mainCanvas.setOnMouseClicked(ev->{
                         double[] toDraw = new double[4];
-                        for(double[] x : Node.nodes){
-                            if((ev.getX() >= x[0] - x[3]/2 && ev.getX() <= x[0])&& (ev.getY() >= x[1] - x[3]/2&& ev.getY() <= x[1])){
-                                ctx.strokeLine(finalCurrentNode[0], finalCurrentNode[1],x[0],x[1]);
+                        for(Node x : Node.nodes){
+                            if((ev.getX() >= x.xCord - x.RADIUS && ev.getX() <= x.xCord)&& (ev.getY() >= x.yCord - x.RADIUS&& ev.getY() <= x.yCord)){
+                                ctx.strokeLine(finalCurrentNode.xCord, finalCurrentNode.yCord,x.xCord,x.yCord);
+                                finalCurrentNode.connectedNodes.add(x);
+                                x.connectedNodes.add(finalCurrentNode);
                             }
                         }
                     });
                 }
             });
 
-
-
         drawingText.setText("Currently drawing : ");
         drawingText.setText(drawingText.getText() + " drawing line" );
-    }
-
-    void checkOverlap(){
-
     }
 
     @FXML
     void clearCanvas(){
         Node.nodes.clear();
+        for(Node x : Node.nodes){
+            x.connectedNodes.clear();
+        }
         mainCanvas.setOnMouseClicked(e->{});
         mainCanvas.getGraphicsContext2D().clearRect(0,0,mainCanvas.getWidth(),mainCanvas.getHeight());
 
@@ -84,5 +88,64 @@ public class Controller {
         drawingText.setText(drawingText.getText() + " drawing nothing" );
     }
 
+    @FXML
+    void selectStart(){
+        mainCanvas.setOnMouseClicked(e->{});
+        GraphicsContext ctx = mainCanvas.getGraphicsContext2D();
+        mainCanvas.setOnMouseClicked(e ->{
+            for(Node x : Node.nodes){
+                if((e.getX() >= x.xCord - x.RADIUS && e.getX() <= x.xCord)&& (e.getY() >= x.yCord - x.RADIUS&& e.getY() <= x.yCord)){
+                    start = x;
+                    start_btn.setDisable(true);
+                    x.reDraw(ctx,Color.PURPLE);
+            }}
+        });
 
+    }
+
+    @FXML
+    void bfs(){
+        PriorityQueue<Node> queue = new PriorityQueue<>();
+        int counter = 0;
+        ArrayList<Node> notVisitedNodes = new ArrayList<>();
+        end = Node.nodes.get(4);
+
+        for(Node x : Node.nodes){
+            if(x.isVisited == false) notVisitedNodes.add(x);
+        }
+
+        start.setVisited(true);
+        queue.offer(start);
+        while (queue.size() > 0){
+            Node curr = queue.poll(); //gets the 1st element from que;
+
+            //end condition
+            if(curr == end){
+                break;
+            }
+            for(Node x : curr.connectedNodes){
+               if(x.isVisited() == false){
+                   x.setVisited(true);
+                   x.parent = curr;
+                   queue.offer(x);
+                   counter ++;
+               }
+            }
+        }
+
+        ArrayList<Node> path = new ArrayList<>();
+        path.add(end);
+        Node next = end.parent;
+        while(next != null){
+            path.add(next);
+            next= next.parent;
+        }
+
+        GraphicsContext ctx = mainCanvas.getGraphicsContext2D();
+        for(Node x : path){
+            x.reDraw(ctx,Color.ORANGE);
+        }
+    }
 }
+
+
--- src/sample/Node.java
+++ src/sample/Node.java
@@ -1,30 +1,84 @@
 package sample;
 
+import javafx.geometry.VPos;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class Node {
+public class Node implements Comparable<Node> {
     final int DIAMETER = 50;
     final int RADIUS = DIAMETER/2;
-    static ArrayList<double[]> nodes = new ArrayList<>();
+    static ArrayList<Node> nodes = new ArrayList<>();
+    int id = 0;
+    Node parent;
+    static int counter = 0;
+    ArrayList<Node> connectedNodes = new ArrayList<>();
+    boolean isVisited = false;
+    double xCord;
+    double yCord;
 
-    //location of the node :
+
+//location of the node :
     //x-RADIUS,y-RADIUS,DIAMETER,DIAMETER
     //to check if mouse is overlapping the node I need to check if mouse is anywhere between the above limits
 
     Node(GraphicsContext ctx,double x, double y){
+        this.id = counter++;
         this.draw(ctx,x,y);
-        this.nodes.add(new double[]{x, y, DIAMETER, DIAMETER});
+        this.xCord = x;
+        this.yCord = y;
+        this.nodes.add(this);
+
     }
 
     void draw(GraphicsContext ctx,double x, double y){
         ctx.setFill(Color.LIGHTBLUE);
         ctx.fillOval(x-RADIUS,y-RADIUS,DIAMETER,DIAMETER);
+        ctx.setFill(Color.BLACK);
+        ctx.fillText(Integer.toString(this.id),x-RADIUS,y- RADIUS);
     }
 
-    public ArrayList<double[]> getNodes() {
-        return nodes;
+    void reDraw(GraphicsContext ctx,Color c){
+        ctx.setFill(Color.WHITESMOKE);
+        ctx.fillOval(this.xCord-this.RADIUS,this.yCord-this.RADIUS,this.DIAMETER,this.DIAMETER);
+        ctx.setFill(c);
+        ctx.fillOval(this.xCord-this.RADIUS,this.yCord-this.RADIUS,this.DIAMETER,this.DIAMETER);
+    }
+
+
+    public ArrayList<Node> getConnectedNodes() {
+        return this.connectedNodes;
+    }
+
+    public void addNode (Node n){
+        this.connectedNodes.add(n);
+    }
+
+    public boolean isVisited() {
+        return this.isVisited;
+    }
+
+    public void setVisited(boolean visited) {
+        isVisited = visited;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+    @Override
+    public String toString() {
+       return("" + this.id);
+    }
+
+
+    @Override
+    public int compareTo(Node o) {
+        return 0;
     }
 }
